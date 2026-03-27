dff1a2f4b839ce8fc39376229daa531bfb36525c
CrimsonGames <info.crimsongames@gmail.com>
2026-03-27 18:23:03 +0100

    Update controller.js

--- src/core/controller.js
+++ src/core/controller.js
@@ -1,1 +1,7 @@
-
+export async function generate(input) {
+  const words = input.toLowerCase().split(" ");
+  const { navigate } = await import("./navigator.js");
+  const results = await navigate(words);
+  const { synthesize } = await import("./synthesizer.js");
+  return synthesize(results);
+}
