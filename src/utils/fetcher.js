66386caf10e50f88d2eabb83993d1171e447b4c5
CrimsonGames <info.crimsongames@gmail.com>
2026-03-27 18:24:51 +0100

    Update fetcher.js

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,1 +1,20 @@
+export async function load() {
+  const files = [
+    "../data/htmlStructures/snippet00000001.json",
+    "../data/cssColors/snippet00000002.json"
+  ];
 
+  const data = [];
+
+  for (const file of files) {
+    try {
+      const res = await fetch(file);
+      if (res.ok) {
+        const json = await res.json();
+        data.push(json);
+      }
+    } catch {}
+  }
+
+  return data;
+}
