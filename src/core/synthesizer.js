63a5a51fb742d484b6c1ead54a4ca1f4f7a02f66
CrimsonGames <info.crimsongames@gmail.com>
2026-03-27 18:24:22 +0100

    Update synthesizer.js

--- src/core/synthesizer.js
+++ src/core/synthesizer.js
@@ -1,1 +1,14 @@
+export function synthesize(results) {
+  let html = "<!DOCTYPE html><html><head><style>";
+  let css = "";
+  let body = "</style></head><body>";
 
+  for (const item of results) {
+    if (item.type === "css") css += item.content;
+    if (item.type === "html") body += item.content;
+  }
+
+  body += "</body></html>";
+
+  return html + css + body;
+}
